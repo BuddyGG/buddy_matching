ee5127b6ddcb5f8720764f2911f84d629f9c09ac
ecly <elyne@live.dk>
2017-08-17 20:39:44 +0200

    written regions more concisely and map

--- lib/riot_api/regions.ex
+++ lib/riot_api/regions.ex
@@ -1,40 +1,21 @@
 defmodule LolBuddy.RiotApi.Regions do
   @type region :: :br | :eune | :euw | :jp | :kr | :lan | :las | :na | :oce | :tr | :ru | :pbe
+  @regions %{
+    br: "https://br1.api.riotgames.com",
+    eune: "https://eun1.api.riotgames.com",
+    euw: "https://euw1.api.riotgames.com",
+    jp: "https://jp1.api.riotgames.com",
+    kr: "https://kr.api.riotgames.com",
+    lan: "https://la1.api.riotgames.com",
+    las: "https://la2.api.riotgames.com",
+    na: "https://na1.api.riotgames.com",
+    oce: "https://oc1.api.riotgames.com",
+    tr: "https://tr1.api.riotgames.com",
+    ru: "https://ru.api.riotgames.com",
+    pbe: "https://pbe1.api.riotgames.com"}
 
-  def endpoint(:br) do
-    "https://br1.api.riotgames.com"
+  def endpoint(region) do
+    @regions[region]
   end
-  def endpoint(:eune) do
-    "https://eun1.api.riotgames.com"
-  end
-  def endpoint(:euw) do
-    "https://euw1.api.riotgames.com"
-  end
-  def endpoint(:jp) do
-    "https://jp1.api.riotgames.com"
-  end
-  def endpoint(:kr) do
-    "https://kr.api.riotgames.com"
-  end
-  def endpoint(:lan) do
-    "https://la1.api.riotgames.com"
-  end
-  def endpoint(:las) do
-    "https://la2.api.riotgames.com"
-  end
-  def endpoint(:na) do
-    "https://na1.api.riotgames.com"
-  end
-  def endpoint(:oce) do
-    "https://oc1.api.riotgames.com"
-  end
-  def endpoint(:tr) do
-    "https://tr1.api.riotgames.com"
-  end
-  def endpoint(:ru) do
-    "https://ru.api.riotgames.com"
-  end
-  def endpoint(:pbe) do
-    "https://pbe1.api.riotgames.com"
-  end
+
 end
---
+++ lib/riot_api/riot_api.ex
@@ -0,0 +1,54 @@
+defmodule LolBuddy.RiotApi.Api do
+  alias LolBuddy.RiotApi.Regions, as: Region
+  @key "RGAPI-7b1921bd-f9f9-4acc-bf12-30007df4e54b"
+
+  defp handle_json({:ok, %{status_code: 200, body: body}}) do
+    Poison.Parser.parse!(body)
+  end
+
+  defp handle_json({_, %{status_code: _, body: _}}) do
+    IO.puts "Check if region or summoner name is wrong"
+  end
+
+  defp summoner_url(name, region) do
+    Region.endpoint(region) <> "/lol/summoner/v3/summoners/by-name/#{name}?api_key=#{@key}"
+  end
+
+  def summoner_id(name, region) do
+    summoner_url(name, region) 
+    |> HTTPoison.get
+    |> handle_json
+    |> Map.get("id")
+  end 
+
+  defp leagues_url(id, region) do
+    Region.endpoint(region) <> "/lol/league/v3/positions/by-summoner/#{id}?api_key=#{@key}"
+  end
+
+  def leagues(name, region) do
+      summoner_id(name, region)
+      |> leagues_url(region) 
+      |> HTTPoison.get
+      |> handle_json
+      |> leagues_extract
+      
+  end 
+
+  defp champions_url(id, region) do
+    Region.endpoint(region) <> "/lol/champion-mastery/v3/champion-masteries/by-summoner/#{id}?api_key=#{@key}"
+  end
+
+  def champions(name, region) do
+    summoner_id(name, region)
+    |> champions_url(region) 
+    |> HTTPoison.get
+    |> handle_json
+  end 
+
+  # Returns a list of tuples with various queue ranking
+  # Example for just one queue> [{"RANKED_SOLO_5x5", "GOLD", "I"}]
+  defp leagues_extract(values) do
+    extract = fn(x) -> {x["queueType"], x["tier"], x["rank"]} end
+    Enum.map(values, extract)
+  end
+end

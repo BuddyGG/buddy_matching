f9c3d9ea6c1e329a8b2090d5d4daa746821cab04
ecly <ecly@mailbox.org>
2017-08-21 18:07:10 +0200

    Now able to get a summoners 3 highest mastery level champions in a neatly formatted way

--- lib/riot_api/riot_api.ex
+++ lib/riot_api/riot_api.ex
@@ -1,6 +1,6 @@
 defmodule LolBuddy.RiotApi.Api do
   alias LolBuddy.RiotApi.Regions, as: Region
-  @key "RGAPI-7b1921bd-f9f9-4acc-bf12-30007df4e54b"
+  @key "RGAPI-65ceef4e-1417-447d-8938-c2b50e2d31c5"
 
   defp handle_json({:ok, %{status_code: 200, body: body}}) do
     Poison.Parser.parse!(body)
@@ -21,6 +21,13 @@ defmodule LolBuddy.RiotApi.Api do
     |> Map.get("id")
   end 
 
+  # Returns a list of tuples with various queue ranking
+  # Example for just one queue> [{"RANKED_SOLO_5x5", "GOLD", "I"}]
+  defp leagues_extract(values) do
+    extract = fn(x) -> {x["queueType"], x["tier"], x["rank"]} end
+    Enum.map(values, extract)
+  end
+
   defp leagues_url(id, region) do
     Region.endpoint(region) <> "/lol/league/v3/positions/by-summoner/#{id}?api_key=#{@key}"
   end
@@ -31,24 +38,31 @@ defmodule LolBuddy.RiotApi.Api do
       |> HTTPoison.get
       |> handle_json
       |> leagues_extract
-      
   end 
+
+  # TODO: Here we could choose the endpoint based on closest to our host
+  def name_from_id(id, region) do
+    Region.endpoint(region) <> "/lol/static-data/v3/champions/#{id}?api_key=#{@key}"
+    |> IO.inspect
+    |> HTTPoison.get
+    |> handle_json
+    |> Map.get("name")
+  end
 
   defp champions_url(id, region) do
     Region.endpoint(region) <> "/lol/champion-mastery/v3/champion-masteries/by-summoner/#{id}?api_key=#{@key}"
   end
 
+  # Returns a list of 3 most played champions as tuples eg.
+  # [{67, Vayne"}, {51, "Caitlyn"}, {81, "Ezreal"}]
   def champions(name, region) do
     summoner_id(name, region)
     |> champions_url(region) 
     |> HTTPoison.get
     |> handle_json
+    |> Enum.take(3)
+    |> Enum.map(fn map -> Map.get(map,"championId") end)
+    |> Enum.map(fn id -> {id, name_from_id(id, region)} end)
   end 
 
-  # Returns a list of tuples with various queue ranking
-  # Example for just one queue> [{"RANKED_SOLO_5x5", "GOLD", "I"}]
-  defp leagues_extract(values) do
-    extract = fn(x) -> {x["queueType"], x["tier"], x["rank"]} end
-    Enum.map(values, extract)
-  end
 end

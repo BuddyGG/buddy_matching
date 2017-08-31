17ce38bbf1e05770e3fbbfdcbc7547545f9e7c97
ecly <ecly@mailbox.org>
2017-08-31 21:27:38 +0200

    finished initial implementation of position deduction from champion name

--- test/riot_api/positions_test.exs
+++ test/riot_api/positions_test.exs
@@ -5,7 +5,19 @@ defmodule LolBuddyRiotApi.PositionsTest do
   test "3 marksmen return marksman only position" do
     champs = ["Vayne", "Ezreal", "Caitlyn"]
     positions = LolBuddy.RiotApi.Positions.positions(champs)
-    assert response == [:marksman]
+    assert positions == [:marksman]
+  end
+
+  test "combination of mid and marksmen" do
+    champs = ["Viktor", "Ezreal", "Caitlyn"]
+    positions = LolBuddy.RiotApi.Positions.positions(champs)
+    assert positions == [:marksman, :mid]
+  end
+
+  test "test that 3 equal weights still return just 2" do
+    champs = ["Viktor", "Kled", "Caitlyn"]
+    positions = LolBuddy.RiotApi.Positions.positions(champs)
+    assert Enum.count(positions) == 2
   end
 
 end

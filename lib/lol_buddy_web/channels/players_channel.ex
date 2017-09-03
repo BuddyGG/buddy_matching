2ec2102a335310051bca4b5a515a7a5df5f397cf
Hans <hanspagh@gmail.com>
2017-09-03 15:09:23 +0200

    implement player mathing in channels

--- lib/lol_buddy_web/channels/players_channel.ex
+++ lib/lol_buddy_web/channels/players_channel.ex
@@ -1,10 +1,12 @@
 defmodule LolBuddyWeb.PlayersChannel do
   use LolBuddyWeb, :channel
+  alias LolBuddy.Player
+  alias LolBuddy.Players
 
   def join("players:lobby", payload, socket) do
     if authorized?(payload) do
       %{"cookie_id" => id} = payload
-      socket = assign(socket, :user, id)
+      socket = assign(socket, :user, %Player{id: id})
       {:ok, socket}
     else
       {:error, %{reason: "unauthorized"}}
@@ -14,9 +16,9 @@ defmodule LolBuddyWeb.PlayersChannel do
   ## On new player event check if the the new players is relevant for the current player
   intercept ["new_player"]
   def handle_out("new_player", payload, socket) do
-    case socket.assigns[:player] == 1 do
-      true -> {:noreply, socket}
-      false -> 
+    case Players.match?(socket.assigns[:user], payload) do
+      false -> {:noreply, socket}
+      true -> 
         push socket, "new_player", payload
         {:noreply, socket}
     end
--- test/lol_buddy_web/channels/players_channel_test.exs
+++ test/lol_buddy_web/channels/players_channel_test.exs
@@ -1,22 +1,29 @@
 defmodule LolBuddyWeb.PlayersChannelTest do
   use LolBuddyWeb.ChannelCase
-
   alias LolBuddyWeb.PlayersChannel
-
+  alias LolBuddy.Player
   setup do
     {:ok, _, socket} =
-      socket("player_socket", %{})
-      |> subscribe_and_join(PlayersChannel, "players:lobby", %{"cookie_id" => 12})
+      socket("", %{})
+      |> subscribe_and_join(PlayersChannel, "players:lobby", %{"cookie_id" => 0})
+    
+    {:ok, socket: socket}
   end
 
-  test "new players are pushed to the client", %{socket: socket} do
-    broadcast_from! socket, "new_player", %{"some" => "data"}
-    assert_push "new_player", %{"some" => "data"}
+  test "new players are pushed to the client when matched", %{socket: socket} do
+    broadcast_from! socket, "new_player", %Player{id: 1}
+    assert_push "new_player", %Player{id: 1}
   end
+  
+  test "new players are not pushed to the client when they dont match", %{socket: socket} do
+    broadcast_from! socket, "new_player", %Player{id: 0}
+    refute_push "new_player", _
+  end
+   
    
   test "unauthenticated users cannot join" do
     error = socket()
-      |> join(AuthorizedChannel, "authorized:lobby")
+      |> join(PlayersChannel, "players:lobby")
 
    assert {:error, %{reason: "unauthorized"}} = error
 end

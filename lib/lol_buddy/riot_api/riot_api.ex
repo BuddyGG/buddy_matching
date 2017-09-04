c215ae14f406cebad2377a9d599a64fab1e472e6
Hans <hanspagh@gmail.com>
2017-09-04 16:03:50 +0200

    update riot api to use static champions instead of getting from the api

--- lib/lol_buddy/riot_api/riot_api.ex
+++ lib/lol_buddy/riot_api/riot_api.ex
@@ -1,13 +1,14 @@
 defmodule LolBuddy.RiotApi.Api do
   alias LolBuddy.RiotApi.Regions, as: Region
   alias LolBuddy.RiotApi.Positions, as: Position
+  alias LolBuddy.RiotApi.Champions
 
   defp handle_json({:ok, %{status_code: 200, body: body}}) do
     {:ok ,Poison.Parser.parse!(body)}
   end
 
-  defp handle_json({_, %{status_code: _, body: _}}) do
-    {:error, "Check if region or summoner name is wrong"}
+  defp handle_json({_, %{status_code: _, body: body}}) do
+    {:error, body}
   end
 
   defp summoner_url(name, region) do
@@ -25,7 +26,7 @@ defmodule LolBuddy.RiotApi.Api do
     summoner_url(name, region) 
     |> parse_json
     |> case do
-        {:ok, summoner} -> summoner|> Map.get("id")
+        {:ok, summoner} -> {:ok, summoner |> Map.get("id")}
         {:error, error} -> {:error, error}
     end
   end 
@@ -43,24 +44,19 @@ defmodule LolBuddy.RiotApi.Api do
   end
 
   def leagues(name, region) do
-    with id <- summoner_id(name, region),
+    with {:ok, id} <- summoner_id(name, region),
          {:ok, league} <- leagues_url(id, region) |> parse_json()
     do
-      league |> leagues_extract
+      league = league |> leagues_extract
+      {:ok, league}
     else 
       {:error, error} -> {:error, error}
     end
   end 
 
   # TODO: Here we could choose the endpoint based on closest to our hostserver
-  def name_from_id(id, region) do
-    key = Application.fetch_env!(:lol_buddy, :riot_api_key) 
-    Region.endpoint(region) <> "/lol/static-data/v3/champions/#{id}?api_key=#{key}"
-    |> parse_json
-    |> case do
-        {:ok, summoner} -> summoner|> Map.get("name")
-        {:error, error} -> {:error, error}
-    end
+  def name_from_id(id) do
+    Champions.find_by_id(id).name
   end
 
   defp champions_url(id, region) do
@@ -71,13 +67,15 @@ defmodule LolBuddy.RiotApi.Api do
   # Returns a list of 3 most played champions as tuples eg.
   # [{67, Vayne"}, {51, "Caitlyn"}, {81, "Ezreal"}]
   def champions(name, region) do
-    with id <- summoner_id(name, region),
-        {:ok, champions} <- champions_url(id, region) |> parse_json()
+    with {:ok, id} <- summoner_id(name, region),
+         {:ok, champions} <- champions_url(id, region) |> parse_json()
     do
-      champions
+      champions = champions
       |> Enum.take(3)
       |> Enum.map(fn map -> Map.get(map,"championId") end)
-      |> Enum.map(fn id -> {id, name_from_id(id, region)} end)
+      |> Enum.map(fn id -> %{id: id, name: name_from_id(id)} end)
+
+      {:ok, champions}
     else
       {:error, error} -> {:error, error}
     end
@@ -86,13 +84,19 @@ defmodule LolBuddy.RiotApi.Api do
   # Returns a map containing name, region, champions and positions
   # for the given summoner in the given region
   def get_summoner_info(name, region) do 
-    champions = champions(name, region)
-    leagues = leagues(name, region)
-    positions = Position.positions(Keyword.values(champions))
-    %{"name" => name,
-      "region" => region,
-      "champions" => champions,
-      "leagues" => leagues,
-      "positions" => positions}
+    with {:ok, champions} <- champions(name, region),
+         {:ok, leagues} <- leagues(name, region)
+    do 
+      positions = champions |> Enum.map(fn (x) -> x.name end) |> Position.positions()
+      data = %{name: name,
+      region: region,
+      champions: champions,
+      leagues: leagues,
+      positions: positions}
+
+      {:ok, data}
+    else 
+      {:error, error} -> {:error, error}
+    end
   end
 end

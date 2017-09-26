0354099752a461de5542f640ab7fa62d7679c547
ecly <ecly@mailbox.org>
2017-09-26 12:43:55 +0200

    superfluous alias, and testing

--- test/player_server/player_server_test.exs
+++ test/player_server/player_server_test.exs
@@ -15,4 +15,29 @@ defmodule LolBuddy.PlayerServerTest do
     PlayerServer.add(server, player)
     assert [player] = PlayerServer.read(server)
   end
+
+  test "players are added", %{server: server} do
+    assert PlayerServer.read(server) == []
+
+    player1 = %Player{id: 1}
+    player2 = %Player{id: 2}
+    PlayerServer.add(server, player1)
+    assert [player1] = PlayerServer.read(server)
+
+    PlayerServer.add(server, player2)
+    assert length(PlayerServer.read(server)) == 2
+  end
+
+  @tag :pending
+  test "players may not be added twice", %{server: server} do
+    assert PlayerServer.read(server) == []
+    player1 = %Player{id: 1}
+
+    PlayerServer.add(server, player1)
+    assert [player1] = PlayerServer.read(server)
+
+    PlayerServer.add(server, player1)
+    assert [player1] = PlayerServer.read(server)
+  end
+
 end

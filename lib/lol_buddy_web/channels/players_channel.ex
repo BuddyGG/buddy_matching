68dc64a1f43f2b2a7d56ee6665d232db01573694
Hans <hanspagh@gmail.com>
2017-09-27 16:19:49 +0200

    update channels to specifications

--- lib/lol_buddy_web/channels/players_channel.ex
+++ lib/lol_buddy_web/channels/players_channel.ex
@@ -3,40 +3,37 @@ defmodule LolBuddyWeb.PlayersChannel do
   alias LolBuddy.Players.Player
   alias LolBuddy.Players
 
-  def join("players:lobby", payload, socket) do
-    if authorized?(payload) do
-      %{"cookie_id" => id} = payload
-      socket = assign(socket, :user, %Player{id: id})
+
+  @doc """
+  Each clients joins their own player channel players:cookie_id 
+  """
+  #TODO auth users
+  def join("players:" <> cookie_id, _, socket) do
+      socket = assign(socket, :user, %Player{id: cookie_id})
       send(self(), {:on_join, {}})
       {:ok, socket}
-    else
-      {:error, %{reason: "unauthorized"}}
-    end
   end
 
+  @doc """
+  On join we find players mathing the newly joined player,
+  return the list of mathing players to the newly joined player
+  and notifies each of the mathing players about the newly joined players aswell
+  """
   def handle_info({:on_join, _msg}, socket) do
-    broadcast! socket, "new_player", socket.assigns[:user]
-    push socket, "new_players", %{players: Players.find_matches(socket.assigns[:user], [])}
+    #TODO ask the genserver for joined players
+    matching_players = Players.find_matches(socket.assigns[:user], [])
+    #TODO store the the newly joined player in the genserver
+
+    #Send all macthin players
+    push socket, "new_players", %{players: matching_players}
+    
+    #Send the newly joined user to all matching players
+    Enum.each(matching_players, fn player ->
+      LolBuddyWeb.Endpoint.broadcast! "player:"<> player.id, "new_player", socket.assigns[:user,]
+    end)
+
+    
     {:noreply, socket}
   end
 
-  ## On new player event check if the the new players is relevant for the current player
-  intercept ["new_player"]
-  def handle_out("new_player", payload, socket) do
-    case Players.match?(socket.assigns[:user], payload) do
-      false -> {:noreply, socket}
-      true -> 
-        push socket, "new_player", payload
-        {:noreply, socket}
-    end
-  end
-
-  ## ensure that only users with a id can join
-  defp authorized?(%{"cookie_id" => _id}) do
-    true
-  end
-  
-  defp authorized?(_payload) do
-    false
-  end
 end

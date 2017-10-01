281c93ffe40bda7869341acddc1ca4a57135ee5a
ecly <ecly@mailbox.org>
2017-10-01 17:02:59 +0200

    named and supervised registration of different region playerservers

--- lib/lol_buddy/region_mapper/region_mapper.ex
+++ lib/lol_buddy/region_mapper/region_mapper.ex
@@ -2,21 +2,11 @@ defmodule LolBuddy.RegionMapper do
   alias LolBuddy.Players.Player
   alias LolBuddy.PlayerServer
 
-  def lookup(server, region) do
-    GenServer.call(server, {:lookup, region})
-  end
-
   def get_players(region) do
-    case lookup(PlayerServer, region) do
-    {:ok, server} -> PlayerServer.read(server)
-          _ -> nil
-    end
+    PlayerServer.read(region)
   end
 
   def add_player(%Player{} = player) do
-    case lookup(PlayerServer, player.region) do
-    {:ok, server} -> PlayerServer.add(server, player)
-          _ -> nil
-    end
+    PlayerServer.add(player.region, player)
   end
 end

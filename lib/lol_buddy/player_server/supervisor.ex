0a2ef91db255f781b3f6132bfa8ccb006225c275
ecly <ecly@mailbox.org>
2017-10-01 17:02:59 +0200

    named and supervised registration of different region playerservers

--- lib/lol_buddy/player_server/supervisor.ex
+++ lib/lol_buddy/player_server/supervisor.ex
@@ -1,16 +1,28 @@
 defmodule LolBuddy.PlayerServer.Supervisor do
   use Supervisor
+  alias LolBuddy.PlayerServer
 
-  def start_link do
-    Supervisor.start_link(__MODULE__, :ok, name: __MODULE__)
+  def start_link(opts) do
+    Supervisor.start_link(__MODULE__, :ok, opts)
   end
 
   def init(:ok) do
     children = [
-      worker(LolBuddy.PlayerServer, [], restart: :transient)
+      Supervisor.child_spec({PlayerServer, name: :br}, id: :worker_br),
+      Supervisor.child_spec({PlayerServer, name: :eune}, id: :worker_eune),
+      Supervisor.child_spec({PlayerServer, name: :euw}, id: :worker_euw),
+      Supervisor.child_spec({PlayerServer, name: :jp}, id: :worker_jp),
+      Supervisor.child_spec({PlayerServer, name: :kr}, id: :worker_kr),
+      Supervisor.child_spec({PlayerServer, name: :lan}, id: :worker_lan),
+      Supervisor.child_spec({PlayerServer, name: :las}, id: :worker_las),
+      Supervisor.child_spec({PlayerServer, name: :na}, id: :worker_na),
+      Supervisor.child_spec({PlayerServer, name: :oce}, id: :worker_oce),
+      Supervisor.child_spec({PlayerServer, name: :tr}, id: :worker_tr),
+      Supervisor.child_spec({PlayerServer, name: :ru}, id: :worker_ru),
+      Supervisor.child_spec({PlayerServer, name: :pbe}, id: :worker_pbe)
     ]
 
     # only restart the the single broken processor
-    supervise(children, strategy: :simple_one_for_one)
+    Supervisor.init(children, strategy: :one_for_one)
   end
 end

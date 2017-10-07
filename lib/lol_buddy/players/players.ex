9180b18fb6421609f0d0a56c0a5308d2c6edac0d
Hans <hanspagh@gmail.com>
2017-10-07 23:58:25 +0200

    override matching to return all players all the time

--- lib/lol_buddy/players/players.ex
+++ lib/lol_buddy/players/players.ex
@@ -2,6 +2,7 @@ defmodule LolBuddy.Players do
   alias LolBuddy.Players.Matching
 
   def get_matches(player, other_players) do
-    Enum.filter(other_players, &(Matching.match?(player, &1)))
+    #Enum.filter(other_players, &(Matching.match?(player, &1)))
+    other_players
   end
 end

1f40b4e1342c4f5e9ff529dbc836947094ea39ed
Hans <hanspagh@gmail.com>
2017-10-08 12:59:03 +0200

    handle player leave a channel and match requesting

--- test/lol_buddy_web/channels/players_channel_test.exs
+++ test/lol_buddy_web/channels/players_channel_test.exs
@@ -21,24 +21,22 @@ defmodule LolBuddyWeb.PlayersChannelTest do
   leagues: [@diamond1], champions: ["Cho'Gath", "Renekton", "Riven"],
   criteria: @narrow_criteria, comment: "Apparently I play Riven"}
 
-  @base_player3  %Player{id: 3, name: "hansp2", region: :euw, voice: false,
-  languages: ["danish", "english"], age_group: 1, positions: [:top],
-  leagues: [@diamond1], champions: ["Cho'Gath", "Renekton", "Riven"],
-  criteria: @narrow_criteria, comment: "Apparently I play Riven"}
-
   
   test "returns other matching players when joining channel and broadcast self as new player" do
-    socket("user:1", %{})
+    {:ok, _, player1} = socket("user:1", %{})
       |> subscribe_and_join(PlayersChannel, "players:#{@base_player1.id}", @base_player1)
-    
+   
     #assert player 1 got no one else
     assert_receive %Phoenix.Socket.Message{
       topic: "players:1",
       event: "new_players",
       payload: %{players: []}}
-    
-    socket("user:2", %{})
+   
+    {:ok, _, player2} =socket("user:2", %{})
     |> join(PlayersChannel, "players:#{@base_player2.id}", @base_player2)
+
+    :ok = close(player1)
+    :ok = close(player2)
     
     #assert player 2 got player 1
     assert_receive %Phoenix.Socket.Message{
@@ -93,13 +91,71 @@ defmodule LolBuddyWeb.PlayersChannelTest do
  })
 
     data = Poison.Parser.parse!(player)
-    socket("user:1", %{})
+    {:ok, _, player1} = socket("user:1", %{})
       |> subscribe_and_join(PlayersChannel, "players:1", %{"payload" => data})
+      
+    :ok = close(player1)
     
       assert_receive %Phoenix.Socket.Message{
       topic: "players:1",
       event: "new_players",
       payload: %{players: []}}
+  end
+
+  test "player can request to match with an other player" do
+    {:ok, _, player1} = socket("test2", %{})
+    |> subscribe_and_join(PlayersChannel, "players:#{@base_player1.id}", @base_player1)
+   
+    {:ok, _, player2} = socket("test2", %{})
+    |> join(PlayersChannel, "players:#{@base_player2.id}", @base_player2)
+    
+    player1 
+    |> push("request_match", %{"player" => @base_player2})
+
+    :ok = close(player1)
+    :ok = close(player2)
+    
+    #Both players should recive the match request
+    assert_receive %Phoenix.Socket.Message{
+      topic: "players:1",
+      event: "match_requested",
+      payload: @base_player2
+    }
+
+    assert_receive %Phoenix.Socket.Message{
+      topic: "players:2",
+      event: "match_requested",
+      payload: @base_player1
+    }
+      
+  end
+  
+  test "player can respond to match request" do
+    {:ok, _, player1} = socket("test2", %{})
+    |> subscribe_and_join(PlayersChannel, "players:#{@base_player1.id}", @base_player1)
+   
+    {:ok, _, player2} = socket("test2", %{})
+    |> join(PlayersChannel, "players:#{@base_player2.id}", @base_player2)
+    
+    player1 
+    |> push("respond_to_request", %{"id" => @base_player2.id, "responds" => "accepted"})
+    
+    :ok = close(player1)
+    :ok = close(player2)
+    
+    #Both players should recive the request response from player1
+    assert_receive %Phoenix.Socket.Message{
+      topic: "players:1",
+      event: "request_responds",
+      payload: %{responds: "accepted"}
+    }
+
+    assert_receive %Phoenix.Socket.Message{
+      topic: "players:2",
+      event: "request_responds",
+      payload: %{responds: "accepted"}
+    }
+      
   end
   
  

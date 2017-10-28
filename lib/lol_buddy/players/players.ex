aad5e0330975dd794d3d14e91de873186babe3d9
ecly <ecly@mailbox.org>
2017-10-28 21:31:14 +0200

    made test for update_criteria

--- lib/lol_buddy/players/players.ex
+++ lib/lol_buddy/players/players.ex
@@ -2,7 +2,6 @@ defmodule LolBuddy.Players do
   alias LolBuddy.Players.Matching
 
   def get_matches(player, other_players) do
-    #Enum.filter(other_players, &(Matching.match?(player, &1)))
-    other_players
+    Enum.filter(other_players, &(Matching.match?(player, &1)))
   end
 end

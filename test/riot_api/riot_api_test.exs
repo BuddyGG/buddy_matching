54d76727f191f95ccd9824ef1ce7f857dc981891
ecly <ecly@mailbox.org>
2017-11-14 13:29:10 +0100

    added testing for extract most played

---
+++ test/riot_api/riot_api_test.exs
@@ -0,0 +1,40 @@
+defmodule LolBuddyRiotApi.ApiTest do
+  require LolBuddy.RiotApi.Api
+  use ExUnit.Case, async: true
+
+  test "correct champions are extracted from matches" do
+    matches =
+      [%{"champion" => 24, "lane" => "BOTTOM", "role" => "DUO_SUPPORT"},
+       %{"champion" => 24, "lane" => "BOTTOM", "role" => "DUO_SUPPORT"},
+       %{"champion" => 37, "lane" => "BOTTOM", "role" => "DUO_SUPPORT"},
+       %{"champion" => 37, "lane" => "BOTTOM", "role" => "DUO_SUPPORT"},
+       %{"champion" => 18, "lane" => "BOTTOM", "role" => "DUO_SUPPORT"},
+       %{"champion" => 18, "lane" => "BOTTOM", "role" => "DUO_SUPPORT"},
+       %{"champion" => 27, "lane" => "BOTTOM", "role" => "DUO_SUPPORT"}]
+    most_played = LolBuddy.RiotApi.Api.extract_most_played(matches)
+    assert Enum.member?(most_played, "Jax")
+    assert Enum.member?(most_played, "Sona")
+    assert Enum.member?(most_played, "Tristana")
+    assert Enum.count(most_played) == 3
+  end
+
+  test "draws still return at most 3 champions" do
+    matches =
+      [%{"champion" => 24, "lane" => "BOTTOM", "role" => "DUO_SUPPORT"},
+       %{"champion" => 37, "lane" => "BOTTOM", "role" => "DUO_SUPPORT"},
+       %{"champion" => 18, "lane" => "BOTTOM", "role" => "DUO_SUPPORT"},
+       %{"champion" => 27, "lane" => "BOTTOM", "role" => "DUO_SUPPORT"}]
+    most_played = LolBuddy.RiotApi.Api.extract_most_played(matches)
+    assert Enum.count(most_played) == 3
+  end
+
+  test "less than 3 champions in matches still finds most played" do
+    matches =
+      [%{"champion" => 24, "lane" => "BOTTOM", "role" => "DUO_SUPPORT"},
+       %{"champion" => 24, "lane" => "BOTTOM", "role" => "DUO_SUPPORT"},
+       %{"champion" => 24, "lane" => "BOTTOM", "role" => "DUO_SUPPORT"}]
+    most_played = LolBuddy.RiotApi.Api.extract_most_played(matches)
+    assert Enum.member?(most_played, "Jax")
+    assert Enum.count(most_played) == 1
+  end
+end

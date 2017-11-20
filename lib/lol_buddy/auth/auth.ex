60e0adbb6d92b0c1be430771eb85c863f8a83bc7
Hans <hanspagh@gmail.com>
2017-11-20 23:58:46 +0100

    add docs to auth module

--- lib/lol_buddy/auth/auth.ex
+++ lib/lol_buddy/auth/auth.ex
@@ -1,21 +1,46 @@
 defmodule LolBuddy.Auth do
-    @salt "session"
-    #Tokens are valid for 30 days
-    @max_age 86400 * 30
+  @moduledoc """
+  Authenticate module using Phoenix tokens
+  Used to generate session ids, tokens and verifying these
+  """
 
-    def generate_session_id do
-        UUID.uuid4() 
-    end
+  @salt "session"
 
-    def generate_session_token(session_id) do
-        Phoenix.Token.sign(LolBuddyWeb.Endpoint, @salt, session_id, max_age: @max_age )
-    end
+  #Tokens are valid for 30 days
+  @max_age 86400 * 30
 
-    def verify_session(session_id, session_token) do
-        case Phoenix.Token.verify(LolBuddyWeb.Endpoint, @salt, session_token, max_age: @max_age) do
-            {:ok, id_from_token} -> id_from_token == session_id
-            {:error, _error } -> false
-        end
-    end
+  @doc """
+  Generate a valid uuid session id.
+  ## Example:
+    iex> LolBuddy.Auth.generate_session_id
+      "fe8d2ecb-38d3-4b17-a745-b455ce78183b"
+  """
+  def generate_session_id do
+      UUID.uuid4() 
+  end
+
+  @doc """
+  Sign a session id to a create session token with a max_age of 30 days 
+  ## Example
+    iex> LolBuddy.Auth.generate_session_token("fe8d2ecb-38d3-4b17-a745-b455ce78183b")
+      "SFMyNTY.g3QAAAACZAAEZGF0YW0AAAAkZmU4ZDJlY2ItMzhkMy00YjE3LWE3NDUtYjQ1NWNlNzgxODNiZAAGc2lnbmVkbgYAa_yf218B.a4U-ibqtnyFogL_LN9EmkDruXUuT4S_r--U6twFZSqo"
+  """
+  def generate_session_token(session_id) do
+      Phoenix.Token.sign(LolBuddyWeb.Endpoint, @salt, session_id, max_age: @max_age )
+  end
+
+  @doc """
+  Verify a session id/token pair
+  ## Example
+    iex> LolBuddy.Auth.verify_session("fe8d2ecb-38d3-4b17-a745-b455ce78183b",
+    "SFMyNTY.g3QAAAACZAAEZGF0YW0AAAAkZmU4ZDJlY2ItMzhkMy00YjE3LWE3NDUtYjQ1NWNlNzgxODNiZAAGc2lnbmVkbgYAa_yf218B.a4U-ibqtnyFogL_LN9EmkDruXUuT4S_r--U6twFZSqo")
+      true
+  """
+  def verify_session(session_id, session_token) do
+      case Phoenix.Token.verify(LolBuddyWeb.Endpoint, @salt, session_token, max_age: @max_age) do
+          {:ok, id_from_token} -> id_from_token == session_id
+          {:error, _error } -> false
+      end
+  end
 
 end

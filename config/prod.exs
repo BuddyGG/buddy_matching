3825adb1c7f2e309cca509aab45ec2d55d212e34
Hans <hanspagh@gmail.com>
2017-12-28 12:57:32 +0100

    configs for digital ocean

--- config/prod.exs
+++ config/prod.exs
@@ -16,10 +16,8 @@ use Mix.Config
 config :lol_buddy, LolBuddyWeb.Endpoint,
   load_from_system_env: true,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "lolbuddy.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "api.lolbuddy.tk", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  #todo remember to change this
-  check_origin: false,
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 config :lol_buddy, riot_api_key: System.get_env("RIOT_API_KEY")

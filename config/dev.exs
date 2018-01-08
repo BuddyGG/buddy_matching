06e0de763d3b3b7d40fe5f9ecd3199d73aed0304
Hans <hanspagh@gmail.com>
2018-01-08 21:45:28 +0100

    add conform as a configuration manager to handle env vars much nicer

--- config/dev.exs
+++ config/dev.exs
@@ -30,7 +30,7 @@ config :lol_buddy, LolBuddyWeb.Endpoint,
 
 # Do not include metadata nor timestamps in development logs
 config :logger, :console, format: "[$level] $message\n"
-
+config :logger, level: :debug
 # Set a higher stacktrace during development. Avoid configuring such
 # in production as building large stacktraces may be expensive.
 config :phoenix, :stacktrace_depth, 20

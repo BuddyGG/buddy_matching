132270345a04635871cdd90551df534ed04fa9ed
ecly <ecly@mailbox.org>
2018-03-04 14:31:12 +0100

    bump mix.exs version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule LolBuddy.Mixfile do
   def project do
     [
       app: :lol_buddy,
-      version: "0.0.1",
+      version: "1.1.1",
       elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),

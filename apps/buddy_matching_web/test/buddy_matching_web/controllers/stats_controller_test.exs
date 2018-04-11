3c7e25cb2c9937d5a68405b171c28f9c82432d71
Hans <hanspagh@gmail.com>
2018-04-12 00:08:37 +0200

    add stats controller and route

---
+++ apps/buddy_matching_web/test/buddy_matching_web/controllers/stats_controller_test.exs
@@ -0,0 +1,53 @@
+defmodule BuddyMatching.StatsControllerTest do
+  use BuddyMatchingWeb.ConnCase
+  alias BuddyMatching.PlayerServer.RegionMapper
+  alias BuddyMatching.Players.Player
+
+  test "1 connected player to a euw server returns 1 player" do
+    player = %Player{id: "1", name: "foo", region: :euw}
+    RegionMapper.add_player(player)
+
+    conn = build_conn()
+
+    players_online =
+      conn
+      |> get(stats_path(conn, :show))
+      |> json_response(200)
+      |> Map.get("players_online")
+
+    # players should leave agin to leave no trace
+    RegionMapper.remove_player(player)
+
+    assert players_online["euw"] == 1
+
+    # all other regions should be 0
+    players_online
+    |> Map.delete("euw")
+    |> Enum.all?(fn {_server, count} -> count == 0 end)
+    |> assert
+  end
+
+  test "1 connected player to a br server returns 1 player" do
+    player = %Player{id: "1", name: "foo", region: :br}
+    RegionMapper.add_player(player)
+
+    conn = build_conn()
+
+    players_online =
+      conn
+      |> get(stats_path(conn, :show))
+      |> json_response(200)
+      |> Map.get("players_online")
+
+    # players should leave agin to leave no trace
+    RegionMapper.remove_player(player)
+
+    assert players_online["br"] == 1
+
+    # all other regions should be 0
+    players_online
+    |> Map.delete("br")
+    |> Enum.all?(fn {_server, count} -> count == 0 end)
+    |> assert
+  end
+end

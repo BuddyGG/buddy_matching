5af5bdf6308ef00ba44d0a8ec76e43f62d364be5
ecly <ecly@mailbox.org>
2018-04-13 00:14:44 +0200

    intial working access_server for token handling.
    Refresh token does not work yet, and it fails when ran in test,
    as it in its init function currently depends on environment variables,
    as it already then tries to fetch an access token.
    
    Should probably just instantiate it with dummy state, where the
    expiration date, is the small DateTime, such that first call to the
    server triggers the token fetch. Yet to be discovered why refresh token
    fails.
    
    May also be beneficial to add a task for pinging the AccessServer with some
    certain interval, as the refresh_token can expire otherwise.

--- apps/fortnite_api/lib/fornite_api/stats.ex
+++ apps/fortnite_api/lib/fornite_api/stats.ex
@@ -8,13 +8,28 @@ defmodule FortniteApi.Stats do
   @duo "p10"
   @squad "p9"
 
+  @doc """
+  Formats the list of maps returned by Fornite's API
+  as a Map of %{"stat_type" => "value}.
+
+  Examples:
+    iex> format_stats([%{"name" => "br_score_pc_m0_p10", "value" => 703}])
+    %{"br_score_pc_m0_p10" => 703}
+  """
   def format_stats(stats) do
-    stats
-    |> Enum.map(&{Map.get(&1, "name"), Map.get(&1, "value")})
-    |> Enum.into(%{})
+    Enum.reduce(stats, %{}, fn x, acc -> Map.put(acc, x["name"], x["value"]) end)
   end
 
   # Expects the given stats to be formatted
+  @doc """
+  Extracts specific stats from a formatted stats map, a platform
+  and a queue type and returns them as a tuple:
+
+  Examples:
+    iex> stats = %{"br_placetop1_p2_pc" => 1, "br_placetop3_p2" => 3...}
+    iex> get_stats_for_queue(stats, "pc", "p2")
+    {5, 10, 1, 3, 7}
+  """
   def get_stats_for_queue(stats, platform, queue) do
     wins = Map.get(stats, "br_placetop1_#{platform}_m0_#{queue}", 0)
     top3 = Map.get(stats, "br_placetop3_#{platform}_m0_#{queue}", 0)
@@ -41,14 +56,18 @@ defmodule FortniteApi.Stats do
     total_wins = wins_solo + wins_duo + wins_squad
 
     %{
-      "gamesPlayed" => games_duo,
-      "gamesWon" => wins_duo,
-      "totalGamesPlayed" => total_games,
-      "totalGamesWon" => total_wins,
-      "top1finishes" => wins_duo,
-      "top3finishes" => top3_duo,
-      "top5finishes" => top5_duo,
-      "killDeathRatio" => kdr_duo
+      "total" => %{
+        "totalGamesPlayed" => total_games,
+        "totalGamesWon" => total_wins
+      },
+      "duo" => %{
+        "gamesPlayed" => games_duo,
+        "gamesWon" => wins_duo,
+        "top1finishes" => wins_duo,
+        "top3finishes" => top3_duo,
+        "top5finishes" => top5_duo,
+        "killDeathRatio" => kdr_duo
+      }
     }
   end
 end

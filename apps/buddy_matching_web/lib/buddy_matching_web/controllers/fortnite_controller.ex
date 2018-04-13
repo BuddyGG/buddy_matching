906a2dfd0c3b66499006fb23d6596770e1d3a23a
ecly <ecly@mailbox.org>
2018-04-13 11:20:02 +0200

    integrate fortnite_api with buddy_matching_web

---
+++ apps/buddy_matching_web/lib/buddy_matching_web/controllers/fortnite_controller.ex
@@ -0,0 +1,19 @@
+defmodule BuddyMatchingWeb.FortniteController do
+  use BuddyMatchingWeb, :controller
+  action_fallback(BuddyMatchingWeb.FallbackController)
+
+  alias FortniteApi
+  alias BuddyMatchingWeb.FortniteView
+
+  @doc """
+  Get request to stats for a fortnite player on a specific platform
+  """
+  def show(conn, %{"platform" => platform, "name" => name}) do
+    name
+    |> FortniteApi.fetch_stats(platform)
+    |> case do
+      {:ok, stats} -> render(conn, "show.json", fortnite: stats)
+      {:error, error} -> render(conn, BuddyMatchingWeb.ErrorView, "error.json", error: error)
+    end
+  end
+end

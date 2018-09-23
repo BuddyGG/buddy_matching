53b46e3c80e724dcce56684c1ef2c4a71126643b
ecly <ecly@mailbox.org>
2018-09-23 15:03:46 +0200

    fortnite game specific match testing based solely on platform

--- apps/buddy_matching/test/players/matching/fortnite_matching_test.exs
+++ apps/buddy_matching/test/players/matching/fortnite_matching_test.exs
@@ -4,9 +4,15 @@ defmodule BuddyMatching.Matching.FortniteMatchingTest do
   # alias BuddyMatching.Players.Criteria.FortniteCriteria
   alias BuddyMatching.Players.Matching.FortniteMatching, as: Matching
 
-  test "fortnite matching test" do
+  test "fortnite players match when on same platform" do
     p1 = %FortniteInfo{platform: :xb1}
     p2 = %FortniteInfo{platform: :xb1}
     assert Matching.match?(p1, p2)
+  end
+
+  test "fortnite players do not match if on different platforms" do
+    p1 = %FortniteInfo{platform: :ps4}
+    p2 = %FortniteInfo{platform: :xb1}
+    refute Matching.match?(p1, p2)
   end
 end

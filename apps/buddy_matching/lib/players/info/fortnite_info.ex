37248214df1fb654e0efa69c42e40f1698c72530
ecly <ecly@mailbox.org>
2018-11-18 11:43:24 +0100

    implement fortnite info and criteria json parsing

--- apps/buddy_matching/lib/players/info/fortnite_info.ex
+++ apps/buddy_matching/lib/players/info/fortnite_info.ex
@@ -5,6 +5,8 @@ defmodule BuddyMatching.Players.Info.FortniteInfo do
   Implements `FromJsonBehaviour`.
   """
 
+  require OK
+  alias BuddyMatching.Players.Criteria.FortniteCriteria
   alias BuddyMatching.Players.FromJsonBehaviour
   @behaviour FromJsonBehaviour
 
@@ -14,15 +16,38 @@ defmodule BuddyMatching.Players.Info.FortniteInfo do
 
   @platforms %{"pc" => :pc, "ps4" => :ps4, "xbox" => :xb1}
 
-  def from_json(data) do
-    if Map.has_key?(@platforms, data["platform"]) do
+  @doc """
+  Validates the given JSON map, and passes the %FortniteInfo{} portion thereof.
+  As such, the returned %FortniteInfo, will have `nil` for :game_criteria.
+
+  Returns `%{:ok, %FortniteInfo{}}` || `{:error, reason}`.
+  """
+  def fortnite_info_from_json(data) do
+    if Map.has_key?(@platforms, Map.get(data, "platform")) do
       {:ok,
        %BuddyMatching.Players.Info.FortniteInfo{
-         platform: @platforms[data["platform"]]
+         platform: @platforms[data["platform"]],
+         games_played: data["gamesPlayed"]
        }}
     else
       platforms = Map.keys(@platforms)
       {:error, "Platform should be one of #{inspect(platforms)}"}
+    end
+  end
+
+  @doc """
+  Parses a %FortniteInfo{} struct from a parsed JSON map.
+  This includes some error handling, such as checking whether the given
+  platform is one that we support.The underlying gameCriteria, in
+
+  Returns `{:ok, %FortniteInfo{}}` || `{:error, reason}`
+  """
+  def from_json(data) do
+    OK.for do
+      info <- fortnite_info_from_json(data)
+      criteria <- FortniteCriteria.from_json(data["gameCriteria"])
+    after
+      %BuddyMatching.Players.Info.FortniteInfo{info | game_criteria: criteria}
     end
   end
 end
--- apps/buddy_matching/test/players/info/fortnite_info_test.exs
+++ apps/buddy_matching/test/players/info/fortnite_info_test.exs
@@ -1,19 +1,33 @@
 defmodule BuddyMatching.Info.FortniteInfoTest do
   use ExUnit.Case, async: true
   alias BuddyMatching.Players.Info.FortniteInfo
-  # alias BuddyMatching.Players.Criteria.FortniteCriteria
+  alias BuddyMatching.Players.Criteria.FortniteCriteria
 
   @info_json ~s({
-    "platform": "pc"
+    "platform": "pc",
+    "gamesPlayed": 1
+  })
+
+  @info_json_full ~s({
+    "platform": "pc",
+    "gamesPlayed": 1,
+    "gameCriteria": {
+      "minGamesPlayed": 1
+    }
   })
 
   @info_struct %FortniteInfo{
-    platform: :pc
+    platform: :pc,
+    games_played: 1
   }
 
-  test "entire fortniteinfo is correctly parsed from json" do
+  @info_struct_full %FortniteInfo{
+    @info_struct
+    | game_criteria: %FortniteCriteria{min_games_played: 1}
+  }
+  test "fortnite_info_from_json parses info correctly from json" do
     data = Poison.Parser.parse!(@info_json)
-    assert {:ok, @info_struct} == FortniteInfo.from_json(data)
+    assert {:ok, @info_struct} == FortniteInfo.fortnite_info_from_json(data)
   end
 
   test "parsing fortniteinfo with invalid platform returns an error" do
@@ -22,5 +36,10 @@ defmodule BuddyMatching.Info.FortniteInfoTest do
 
     assert {:error, "Platform should be one of [\"pc\", \"ps4\", \"xbox\"]"} ==
              FortniteInfo.from_json(invalid_data)
+  end
+
+  test "from_json/1 parses fortniteinfo and the underlying criteria" do
+    data = Poison.Parser.parse!(@info_json_full)
+    assert {:ok, @info_struct_full} == FortniteInfo.from_json(data)
   end
 end
